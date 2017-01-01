f35e35cd6350b12c45f678409298013393481f45
Severi Salminen <severi.salminen@iki.fi>
2017-01-01 14:08:54 +0200

    Added error logging to multer.

--- index.js
+++ index.js
@@ -3,21 +3,28 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var multer = require('multer');
-var upload = multer( { dest: 'uploads/' } );
+var upload = multer({dest: 'uploads/'}).single('file');
 
 app.use(express.static(__dirname))
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html');
 });
 
-app.post('/upload', upload.single( 'file' ), function (req, res, next) {
-  console.log("Upload...");
-  console.log(req.body.user_id);
+app.post('/upload', function (req, res) {
+  upload(req, res, function(err) {
+    if(err) {
+      console.log("Multer error!");
+      console.log(err);
+      return;
+    }
+    console.log("Uploading...");
+    console.log(req.body.user_id);
 
-  //res.send('POST request to /upload');
-  return res.status( 200 ).send( req.file );
-})
+    //res.send('POST request to /upload');
+    return res.status( 200 ).send( req.file );
+  });
+});
 
 var room_id = 3;
 
